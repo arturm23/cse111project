c11996b5cc7f4191791979c5b7cb43a15833a4db
arturm23 <musayelyanartur2002@gmail.com>
2023-11-07 10:48:05 -0800

    very cool and awesome queries

--- statements.sql
+++ statements.sql
@@ -12,6 +12,38 @@ SELECT p_name, p_year, ps_height
 FROM player, playerstats 
 WHERE p_name = ps_name;
 
+-- player, what teams they have played for, and their position at the team
+SELECT p_name, p_teamName, ps_position
+FROM player, playerstats
+WHERE p_name = ps_name
+AND p_name = 'player1';
+
+-- display players and their teams throughout the years
+SELECT DISTINCT(p_name), t_name
+FROM player, team
+WHERE p_teamName = t_name;
+
+-- output the teams that have sunny stadiums
+SELECT DISTINCT(t_name), st_stadiumName, st_weather, st_capacity
+FROM team, stadium
+WHERE t_name = st_teamName
+AND st_weather = 'sunny';
+
+-- output the dates of the games where team1 played team2
+SELECT g_date
+FROM game
+WHERE g_home = 'team1'
+AND g_away = 'team2';
+
+-- sort stadiums by capacity
+SELECT st_stadiumName, st_capacity
+FROM stadium
+ORDER BY st_capacity DESC;
+
+-- output the seasons in our database and the winners
+SELECT *
+FROM seasons;
+
 -- "complicated" queries 
 -- output the players of the best team and their stats
 SELECT DISTINCT(p_name), p_teamName, ps_height, ps_weight, ps_position, tr_rank
